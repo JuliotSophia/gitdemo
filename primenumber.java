a2fd33e8e88bc83d401c3becea58b0e61e55b660
JuliotSophia <149039696+JuliotSophia@users.noreply.github.com>
2023-11-01 15:41:27 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,21 @@
+public class primenumber {
+  public static void main(String args[]) {
+
+      int n =5 ;
+
+      int s;
+      int t = 0;
+      for (int i = 2; i <= n/2; i++) {
+          s = n % i;
+          if (s == 0) {
+             t=1;
+          }
+      }
+          if (t== 0) {
+              System.out.println(n + " " + "is a prime number");
+          } else {
+              System.out.println(n + " " + "is  not a prime number");
+          }
+      }
+  }
+
