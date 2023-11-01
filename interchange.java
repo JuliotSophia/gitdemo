b71b5078f56f5a56a4243a8fee26f13bfc63ffeb
Juliot Sophia <juliotsophia@gmail.com>
2023-11-01 14:06:12 +0530

    First java project

---
+++ interchange.java
@@ -0,0 +1,16 @@
+public class interchange {
+    public static void main (String[] args)
+    {
+
+        int a=890;
+        int b=67;
+
+                a=a+b;
+                b=a-b;
+                a=a-b;
+      System.out.println(a+" "+"is the value of a");
+      System.out.println(b+" "+"is the value of b");
+
+    }
+}
+
